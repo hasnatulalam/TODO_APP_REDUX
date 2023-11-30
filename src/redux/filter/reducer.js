d8ce4be5b87dd4656d08aa6dcee29f28f73c88cc
hasnatulalam <hasnatualam056@gmail.com>
2023-11-30 20:04:47 +0600

    first commit

---
+++ src/redux/filter/reducer.js
@@ -0,0 +1,42 @@
+import { STATUSCHANGED,COLORCHANGED } from "./actionTypes";
+import  initialState  from "./initaialState";
+
+
+const reducer =(state=initialState,action)=>{
+   switch (action.type) {
+    case STATUSCHANGED:
+        return {
+            ...state,
+            status:action.payload
+        }
+    case COLORCHANGED:
+        const {color,changeType} =action.payload
+          
+        switch (changeType) {
+            case 'added':
+                return {
+                    ...state,
+                    colors:[
+                        ...state.color,
+                        color
+                    ]
+                }
+
+            case 'removed':
+                return {
+                    ...state,
+                   colors:state.colors.filter(existingColor=> existingColor!==color)
+                }
+                
+               
+        
+            default:
+              return  state;
+        }
+
+   
+        default:
+            return state;
+   }
+}
+export default reducer
---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,17 @@
+  const initialState=[
+    {
+        id:1,
+        text:'learn React',
+        completed:true,
+
+    },
+    {
+        id:2,
+        text:'learn Redux',
+        completed:false,
+        color:'red',
+
+    },
+]
+
+export default initialState
